65353b39661edec3ea5ef535782de1b1b191aeb7
Sawano <aldianokto@gmail.com>
2026-08-08 14:43:47 +0700

    Add addnode endpoint and update RPCServer
    Added a new endpoint for adding nodes and updated the RPCServer structure to include an AddNodeFunc callback.

--- internal/rpc.go
+++ internal/rpc.go
@@ -8,15 +8,17 @@ import (
 
 // RPCServer merepresentasikan peladen HTTP API untuk node XCOSH
 type RPCServer struct {
-	Port   string
-	GetInfoFunc func() map[string]interface{}
+	Port         string
+	GetInfoFunc  func() map[string]interface{}
+	AddNodeFunc  func(addr string) // Callback untuk menangani perintah addnode
 }
 
 // NewRPCServer menginisialisasi instance RPC server baru
-func NewRPCServer(port string, getInfo func() map[string]interface{}) *RPCServer {
+func NewRPCServer(port string, getInfo func() map[string]interface{}, addNode func(addr string)) *RPCServer {
 	return &RPCServer{
 		Port:        port,
 		GetInfoFunc: getInfo,
+		AddNodeFunc: addNode,
 	}
 }
 
@@ -34,6 +36,22 @@ func (s *RPCServer) Start() {
 	// Endpoint health check
 	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("XCOSH Daemon RPC Server is Online!"))
+	})
+
+	// Endpoint baru untuk addnode
+	mux.HandleFunc("/addnode", func(w http.ResponseWriter, r *http.Request) {
+		addr := r.URL.Query().Get("addr")
+		if addr == "" {
+			http.Error(w, "Parameter 'addr' diperlukan", http.StatusBadRequest)
+			return
+		}
+
+		if s.AddNodeFunc != nil {
+			s.AddNodeFunc(addr)
+			fmt.Fprintf(w, "Berhasil mencoba menghubungkan ke peer: %s", addr)
+		} else {
+			http.Error(w, "AddNode handler tidak dikonfigurasi", http.StatusInternalServerError)
+		}
 	})
 
 	fmt.Printf("[+] RPC Server aktif mendengarkan pada port %s...\n", s.Port)
